43fcf53e45ae381fe66e9f7d4399ce0b2364d12e
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-01-27 16:26:07 -0500

    Formatted with black

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 
+
 class LinearRegression:
     """
     A linear regression model that uses close-form solution to fit the model.
@@ -16,7 +17,7 @@ class LinearRegression:
         self.w = None
         self.b = 0
 
-    def fit(self, X:np.ndarray, y:np.ndarray) -> None:
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """Fiting X and y with linear regression model
 
         Args:
@@ -36,9 +37,9 @@ class LinearRegression:
             self.b = params[-1]
         else:
             raise RuntimeError(
-                "LinAlgError. Matrix is Singular. No analytical solution.")
-            
-        
+                "LinAlgError. Matrix is Singular. No analytical solution."
+            )
+
     def predict(self, X: np.ndarray) -> np.ndarray:
         """Predict the output for the given input.
 
@@ -51,10 +52,10 @@ class LinearRegression:
         Returns:
             np.ndarray: Prdiction of target
         """
-        
+
         if self.w is None:
             raise RuntimeError("Model not fitted")
-        
+
         return X @ self.w + self.b
 
 
@@ -62,12 +63,12 @@ class GradientDescentLinearRegression(LinearRegression):
     """
     A linear regression model that uses gradient descent to fit the model.
     """
-    
+
     def _gradient_descent(self, lr: float = 0.0001) -> None:
         with torch.no_grad():
             self.w -= self.w.grad * lr
             self.b -= self.b.grad * lr
-            
+
             # Set gradient to zero
             self.w.grad.zero_()
             self.b.grad.zero_()
@@ -86,11 +87,11 @@ class GradientDescentLinearRegression(LinearRegression):
         # Convert data to tensor
         X = torch.tensor(X).double()
         y = torch.tensor(y).double()
-        
+
         # Initialize w and b as tensor
         self.w = torch.randn(X.shape[1], requires_grad=True, dtype=torch.double)
         self.b = torch.randn(1, requires_grad=True, dtype=torch.double)
-        
+
         # Set criteria for GD
         criteria = torch.nn.MSELoss()
         losses = []
@@ -101,7 +102,7 @@ class GradientDescentLinearRegression(LinearRegression):
             loss.backward()
             self._gradient_descent(lr)
             losses.append(loss.item())
-            
+
         self.w = self.w.detach().numpy()
         self.b = self.b.detach().numpy()
 
@@ -111,7 +112,7 @@ class GradientDescentLinearRegression(LinearRegression):
 
         Arguments:
             X (np.ndarray): The input data.
-            
+
         Raises:
             RuntimeError: Model not fitted
 
