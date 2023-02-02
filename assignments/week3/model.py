bf2ad7f886251b362b92883227d254c0dcc1b246
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-02-02 17:07:06 -0500

    Initial for HW2

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,8 +1,9 @@
 import torch
 from typing import Callable
+import torch.nn as nn
 
-
-class MLP:
+class MLP(nn.Module):
+    """MLP class"""
     def __init__(
         self,
         input_size: int,
@@ -22,9 +23,29 @@ class MLP:
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        
+        self.input_size = input_size
+        self.num_classes = num_classes
+        self.hidden_size = hidden_size
+        self.hidden_count = hidden_count
+        self.activation = activation
+        self.initializer = initializer
+        self.layers = nn.ModuleList()
+        
+        for _ in range(hidden_count):
+            # next_num_inputs = hidden_size
+            layer = nn.Linear(input_size, hidden_size)
+            layer.weight = initializer(layer.weight)
+            self.layers += [layer]
+            input_size = hidden_size
 
-    def forward(self, x):
+        # Create final layer
+        self.out = nn.Linear(input_size, num_classes)
+        self.out.weight = initializer(self.out.weight)
+        
+
+    def forward(self, x:torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +55,14 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        # Flatten inputs to 2D (if more than that)
+        x = x.flatten(end_dim=-2)
+
+        # Get activations of each layer
+        for layer in self.layers:
+          x = self.activation(layer(x))
+
+        # Get outputs
+        x = self.out(x)
+
+        return x
