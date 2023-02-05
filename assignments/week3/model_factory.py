c4206c0c5ab362fc7716a78c93c1e61285800782
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-02-04 20:23:27 -0500

    Tune hyperparameters

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,5 +16,5 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     """
     return MLP(
-        input_dim, 32, output_dim, 4, torch.nn.Tanh, torch.nn.init.xavier_normal_
+        input_dim, 128, output_dim, 10, torch.nn.ReLU, torch.nn.init.xavier_normal_
     )
