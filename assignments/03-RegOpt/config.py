1e46a432690d6f93f0d9728e6e54ba3f02c9eca4
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-02-12 01:39:30 -0500

    First implement HW3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -3,28 +3,30 @@ import torch
 import torch.optim
 import torch.nn as nn
 from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision import transforms
 
 
 class CONFIG:
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 20
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
-    lrs_kwargs = {
-        # You can pass arguments to the learning rate scheduler
-        # constructor here.
-    }
+    lrs_kwargs = {"T": 1000, "eta_min": 1e6}
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
         [
+            # transforms.RandAugment(),
             ToTensor(),
+            Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
         ]
     )
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,10 @@
 from typing import List
-
+import numpy as np
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(self, optimizer, T, eta_min=0, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -12,13 +12,29 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        self.T = T
+        self.eta_min = eta_min
+        self.T_cur = last_epoch
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        return [
+            self.eta_min
+            + (base_lr - self.eta_min) * (1 + np.cos(np.pi * self.T_cur / self.T)) / 2
+            for base_lr in self.base_lrs
+        ]
+
+    def step(self):
+        # Update the current epoch
+        epoch = self.last_epoch + 1
+        self.T_cur = self.T_cur + 1
+
+        # If T_cur overpass T, reset T_cur
+        if self.T_cur >= self.T:
+            self.T_cur = self.T_cur - self.T
+
+        # Update last epoch
+        self.last_epoch = epoch
