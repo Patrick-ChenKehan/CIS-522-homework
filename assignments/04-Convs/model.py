43718640c46aa9e7ca1f0eea68f2a847da537cda
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-03-14 06:02:29 -0400

    Change back

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -8,14 +8,14 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 8, 5)
+        self.conv1 = nn.Conv2d(num_channels, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
         # self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(120, num_classes)
-        self.bn1 = nn.BatchNorm2d(8)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(32)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward function for"""
