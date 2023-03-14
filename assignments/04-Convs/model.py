d5ce1d5903046c4ff560bbe4ed198d4606166a8a
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-03-14 06:45:47 -0400

    Improve

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,10 +10,10 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         self.conv1 = nn.Conv2d(num_channels, 8, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 14, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, num_classes)
         # self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(120, num_classes)
+        # self.fc3 = nn.Linear(120, num_classes)
         self.bn1 = nn.BatchNorm2d(8)
         self.bn2 = nn.BatchNorm2d(16)
 
@@ -22,6 +22,6 @@ class Model(torch.nn.Module):
         x = self.pool(self.bn1(F.relu(self.conv1(x))))
         x = self.pool(self.bn2(F.relu(self.conv2(x))))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = self.fc3(x)
+        # x = F.relu(self.fc1(x))
+        x = self.fc1(x)
         return x
