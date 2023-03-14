f0505484d14066a42d7564bf3e643052d9d78692
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-03-14 05:40:43 -0400

    Reduce fc layer

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -8,14 +8,14 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 32, 5)
+        self.conv1 = nn.Conv2d(num_channels, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.fc1 = nn.Linear(64 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, num_classes)
-        self.bn1 = nn.BatchNorm2d(32)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(16, 32, 5)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
+        # self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(120, num_classes)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(32)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward function for"""
@@ -23,6 +23,6 @@ class Model(torch.nn.Module):
         x = self.pool(self.bn2(F.relu(self.conv2(x))))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
