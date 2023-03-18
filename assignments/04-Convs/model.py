db5785cda0c5ccb514748c8930ce03c7b03539a5
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-03-17 21:55:34 -0400

    improve

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -8,24 +8,24 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.conv1 = nn.Conv2d(num_channels, 8, 5)
+        self.conv1 = nn.Conv2d(num_channels, 8, 3)
         # torch.nn.init.xavier_normal_(self.conv1.weight)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(8, 16, 5)
+        self.conv2 = nn.Conv2d(8, 8, 3)
         # torch.nn.init.xavier_normal_(self.conv2.weight)
-        # self.fc1 = nn.Linear(16 * 6 * 6, 64)
+        self.fc1 = nn.Linear(8 * 6 * 6, 32)
         # torch.nn.init.xavier_normal_(self.fc1.weight)
         # self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(16 * 5 * 5, num_classes)
+        self.fc3 = nn.Linear(32, num_classes)
         self.bn1 = nn.BatchNorm2d(8)
-        self.bn2 = nn.BatchNorm2d(16)
+        self.bn2 = nn.BatchNorm2d(8)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward function for the model"""
         x = self.pool(self.bn1(F.relu(self.conv1(x))))
         x = self.pool(self.bn2(F.relu(self.conv2(x))))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        # x = F.relu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.fc3(x)
         return x
 
