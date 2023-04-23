e9d5e8a2edd35d4e227db9a08c6150687a0b2d27
Patrick-ChenKehan <ChenKehan@sjtu.edu.cn>
2023-04-22 20:53:07 -0400

    Initial commit

--- assignments/05-RL/customagent.py
+++ assignments/05-RL/customagent.py
@@ -1,4 +1,7 @@
 import gymnasium as gym
+import numpy as np
+
+# Write a custom agent for the lunar lander player RL problem
 
 
 class Agent:
@@ -7,9 +10,11 @@ class Agent:
     ):
         self.action_space = action_space
         self.observation_space = observation_space
+        self.q_table = np.zeros((20,) * observation_space.shape[0] + (action_space.n,))
 
     def act(self, observation: gym.spaces.Box) -> gym.spaces.Discrete:
-        return self.action_space.sample()
+        state = self._discretize(observation)
+        return np.argmax(self.q_table[state])
 
     def learn(
         self,
@@ -18,4 +23,31 @@ class Agent:
         terminated: bool,
         truncated: bool,
     ) -> None:
-        pass
+        state = self._discretize(observation)
+        next_state = None
+        action = self.act(observation)
+        if not terminated and not truncated:
+            next_state = self._discretize(observation)
+            self.q_table[state + (action,)] += 0.1 * (
+                reward
+                + 0.9 * np.max(self.q_table[next_state])
+                - self.q_table[state + (action,)]
+            )
+        else:
+            print(terminated, truncated, reward)
+            self.q_table[state + (action,)] += 0.1 * (
+                reward - self.q_table[state + (action,)]
+            )
+
+    def _discretize(self, observation: gym.spaces.Box) -> int:
+        bins = [
+            np.linspace(
+                self.observation_space.low[i], self.observation_space.high[i], num=20
+            )
+            for i in range(self.observation_space.shape[0])
+        ]
+        state = tuple(
+            np.digitize(observation[i], bins[i]) - 1
+            for i in range(self.observation_space.shape[0])
+        )
+        return state
